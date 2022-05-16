255039099a3506a22516441f9e3de9da9f20e625
dingzhenye <zhenye.ding@foxmail.com>
2022-05-16 10:41:31 +0800

    调整cors

--- src/main/java/io/zhenye/crawler/config/WebConfigBeans.java
+++ src/main/java/io/zhenye/crawler/config/WebConfigBeans.java
@@ -11,7 +11,7 @@ public class WebConfigBeans {
 
     private CorsConfiguration corsConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.addAllowedOriginPattern("*");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.setAllowCredentials(true);
