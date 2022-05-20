4ef877a97f22ef18347a558bc935f239632a9537
dingzhenye <zhenye.ding@foxmail.com>
2022-05-20 23:14:35 +0800

    整合xxl-job

---
+++ src/main/java/io/zhenye/crawler/config/XxlJobConfig.java
@@ -0,0 +1,53 @@
+package io.zhenye.crawler.config;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class XxlJobConfig {
+
+    @Value("${xxl.job.admin.addresses}")
+    private String adminAddresses;
+
+    @Value("${xxl.job.accessToken:}")
+    private String accessToken;
+
+    @Value("${xxl.job.executor.app-name}")
+    private String appname;
+
+    @Value("${xxl.job.executor.address:}")
+    private String address;
+
+    @Value("${xxl.job.executor.ip:}")
+    private String ip;
+
+    @Value("${xxl.job.executor.port}")
+    private int port;
+
+    @Value("${xxl.job.executor.log-path}")
+    private String logPath;
+
+    @Value("${xxl.job.executor.log-retention-days}")
+    private int logRetentionDays;
+
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        log.info(">>>>>>>>>>> xxl-job config init.");
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAppname(appname);
+        xxlJobSpringExecutor.setAddress(address);
+        xxlJobSpringExecutor.setIp(ip);
+        xxlJobSpringExecutor.setPort(port);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        xxlJobSpringExecutor.setLogPath(logPath);
+        xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
+        return xxlJobSpringExecutor;
+    }
+
+}
--- src/main/java/io/zhenye/crawler/schedule/CrawlerSchedule.java
+++ src/main/java/io/zhenye/crawler/schedule/CrawlerSchedule.java
@@ -1,41 +1,42 @@
 package io.zhenye.crawler.schedule;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.ObjectUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
 import io.zhenye.crawler.dao.CrawlUrlRepository;
 import io.zhenye.crawler.domain.data.CrawlUrlDO;
 import io.zhenye.crawler.pipeline.DbPipeLine;
-import io.zhenye.crawler.pipeline.RankingPipeLine;
 import io.zhenye.crawler.processor.SmzdmPageProcessor;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.ObjectUtils;
 import org.redisson.api.RAtomicLong;
 import org.redisson.api.RedissonClient;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import us.codecraft.webmagic.Spider;
 
 import java.util.List;
 
-@Slf4j
 @RequiredArgsConstructor
 @Component
 public class CrawlerSchedule {
 
     private final DbPipeLine dbPipeLine;
-    private final RankingPipeLine rankingPipeLine;
     private final CrawlUrlRepository crawlUrlRepository;
     private final RedissonClient redissonClient;
 
     /**
      * 【每小时】12h排行榜
      */
-    @Scheduled(cron = "0 0 * * * ?")
+    @XxlJob("smzdmRankingListSchedule")
     public void smzdmRankingListSchedule() {
-        log.info("[Schedule] smzdmRankingListSchedule start");
-
         List<CrawlUrlDO> crawlUrlSet = crawlUrlRepository.findEnableUrl();
         if (CollUtil.isEmpty(crawlUrlSet)) {
-            log.info("[Schedule] smzdmRankingListSchedule stop. Url is empty");
+            XxlJobHelper.log("[Schedule] smzdmRankingListSchedule stop. Url is empty");
             return;
         }
 
@@ -46,28 +47,39 @@ public class CrawlerSchedule {
             }
         }
         spider.addPipeline(dbPipeLine)
-//                .addPipeline(rankingPipeLine)
                 .thread(4)
                 .run();
-        log.info("[Schedule] smzdmRankingListSchedule end");
+        XxlJobHelper.log("[Schedule] smzdmRankingListSchedule end");
     }
 
-    @Scheduled(cron = "0 */15 * * * ?")
+    @XxlJob("smzdmRankingInitSchedule")
     public void smzdmRankingInitSchedule() {
-        log.info("[Schedule] smzdmRankingInitSchedule start");
+        XxlJobHelper.log("[Schedule] smzdmRankingInitSchedule start");
+
+        // 参数
+        String param = ObjectUtil.defaultIfEmpty(XxlJobHelper.getJobParam(), "{}");
+        JSONObject object = JSON.parseObject(param);
+        int perSize = ObjectUtils.defaultIfNull(object.getInteger("perSize"), 100);
+        JSONArray array = object.getJSONArray("specialPageIds");
 
         Spider spider = Spider.create(new SmzdmPageProcessor());
-        RAtomicLong init = redissonClient.getAtomicLong("init");
-        long start = init.get();
-        long end = start - 100;
-        for (; start > end; start--) {
-            spider.addUrl("https://www.smzdm.com/p/" + start + "/");
+        if (CollectionUtils.isNotEmpty(array)) {
+            for (Object pageId : array) {
+                spider.addUrl("https://www.smzdm.com/p/" + pageId + "/");
+            }
+        } else {
+            RAtomicLong init = redissonClient.getAtomicLong("init");
+            long start = init.get();
+            long end = start - perSize;
+            for (; start > end; start--) {
+                spider.addUrl("https://www.smzdm.com/p/" + start + "/");
+            }
+            init.set(start);
         }
         spider.addPipeline(dbPipeLine)
                 .thread(4)
                 .run();
-        init.set(start);
-        log.info("[Schedule] smzdmRankingInitSchedule end");
+        XxlJobHelper.log("[Schedule] smzdmRankingInitSchedule end");
     }
 
 }
